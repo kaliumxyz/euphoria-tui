ab10f8b7cedcf527caf4d1f58898a23260bf1a07
Martijn Becker <martijn@becker.wtf>
2017-07-23 02:53:10 +0200

    reformatted, restarting this project :3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-import eidolon from './eidolon.js'
+'use strict'
+// const log = e => require('k-log')(e + "\n", "euphoria-cli.log",true)
+const WebSocket = require('ws')
+const color = require("./lib/color")
+const tab = require('./lib/tab')
+const screen = new tab
+const euphoriaConnection = require('euphoria-connection')
+const instantConnection = require('instant-connection')
+
+// dis be all junk ;-;
+
+function handleStdin(data) {
+	if (data.match("!reply")) {
+		let match = data.match(/\d+/)
+		return send('send', { "content": data.substring(9), "parent": repliables[data.match(/\d+/)[0]] })
+	}
+	if (data.match("!who"))
+		return send('who')
+	// send('send',{"content": data})
+}
